e26925607bd18c0558d3f12a9ac28383d25ac5a3
landin_lin <landin.lin@tinklabs.com>
2019-04-30 00:02:06 +0800

    update acknowledge api

--- src/main/java/com/hi/pushmessage/mapper/MessageRecipientsMapper.java
+++ src/main/java/com/hi/pushmessage/mapper/MessageRecipientsMapper.java
@@ -26,9 +26,7 @@ public interface MessageRecipientsMapper extends BaseMapper<MessageRecipients> {
              "       LEFT JOIN devices d ON mr.hotel_id = d.hotel_id " +
              "  AND mr.hotel_room_number = d.hotel_room_number " +
              " " +
-             //"WHERE mi.send_at <= now() " +
              "WHERE 1 = 1 " +
-             "  AND m.action = 'broadcastmessage' " +
              "  AND mr.status IN ('pending', 'sent') " +
              "  AND mi.id = #{messageInfoId} " +
              "  AND d.barcode = #{barcode} " +
